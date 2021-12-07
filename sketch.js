bf9b9101ad60e7c8eaf5d15a7bed832622554aa6
YapJanMei <95682374+YapJanMei@users.noreply.github.com>
2021-12-07 17:14:16 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var box
+function setup() {
+  createCanvas(400,400);
+box = createSprite(200,200,20,20)
+}
+
+function draw() 
+{
+  background("green");
+  if(keyIsDown(RIGHT_ARROW)){
+    box.position.x = box.position.x+10
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    box.position.x = box.position.x-10
+  }
+  if(keyIsDown(UP_ARROW)){
+    box.position.y = box.position.y-10
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    box.position.y = box.position.y+10
+  }
+drawSprites()
+}
+
+
+
+
